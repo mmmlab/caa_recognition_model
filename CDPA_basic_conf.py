6b8c88a56328f55a1853d88eb4a4ec3f66f38f7c
Melchi M. Michel <melchi.michel@rutgers.edu>
2015-06-30 16:46:19 -0400

    Completed everything, including definition of objective function
    - wrote method for computing confidence-based quantiles
    - wrote method for computing empirical freqs & resulting chi-square
    - all methods tested and (apparently) working

--- CDPA_basic_conf.py
+++ CDPA_basic_conf.py
@@ -13,9 +13,9 @@ data_path = 'neha/data/'; # this is the base path for the data files
 # the reason to do this first is that, in order to be efficient,
 # we don't want to represent any more of the time axis than we have to.
 
-remember_hit = numpy.loadtxt(data_path+'remRT_hit.txt'); # load remember RTs for hits
+rem_hit = numpy.loadtxt(data_path+'remRT_hit.txt'); # load remember RTs for hits
 know_hit = numpy.loadtxt(data_path+'knowRT_hit.txt'); # load know RTs for hits
-remember_fa = numpy.loadtxt(data_path+'remRT_fa.txt'); # load remember RTs for false alarms
+rem_fa = numpy.loadtxt(data_path+'remRT_fa.txt'); # load remember RTs for false alarms
 know_fa = numpy.loadtxt(data_path+'knowRT_fa.txt');  # load know RTs for false alarms
 CR = numpy.loadtxt(data_path+'CR.txt');  # load CR RTs 
 miss = numpy.loadtxt(data_path+'miss.txt');  # load miss RTs
@@ -45,14 +45,18 @@ R = 0.1; D = 0.05; L = 0.1; Z = 0.0;
 #param_bounds = [(0.0,1.0),(0.0,1.0),(EPS,1.0),(EPS,1.0),(0.05,1.0),(0.0,1.0),(-1.0,1.0),(-1.0,1.0),(-1.0,1.0)];
 #param_bounds = [(0.51,0.95),(0.1,0.5),(0.0,1.0),(0.0,1.0),(0.05,1.0),(0.05,1,0),(0.1,1.0),(0.0,1.0),(-1.0,1.0),(-1.0,1.0),(-1.0,1.0)];
 #param_bounds = [(0.2,1.0),(0.0,0.19),(0.0,1.0),(0.0,1.0),(EPS,1.0),(EPS,1.0),(0.0,1.0),(0.0,1.0),(-1.0,1.0),(-1.0,1.0),(-1.0,1.0),(0.0,10.0)];
-param_bounds = [(0.2,1.0),(0.01,0.19),(0.0,1.0),(0.0,1.0),(EPS,1.0),(EPS,1.0),(0.0,10.0)];
+#param_bounds = [(0.2,1.0),(0.01,0.19),(0.0,1.0),(0.0,1.0),(EPS,1.0),(EPS,1.0),(0.0,10.0)];
 
 
 fftw.fftw_setup(zeros(NR_SSTEPS),NR_THREADS);
 
+param_bounds = [(0.0,0.1),(0.0,1.0),(0.0,1.0),(EPS,1.0),(EPS,1.0),(0.05,1.0),(0.0,1.0),(-1.0,1.0),(EPS,1.0)];
+# c,mu_r,mu_f,d_r,d_f,tc_bound,r_bound,z0,delta_t = params;
 
 def find_ml_params():
-    return optimize.differential_evolution(compute_chi,param_bounds);
+    obj_func = lambda model_params:compute_model_gof(model_params,rem_hit[:,0],know_hit[:,0],miss[:,0],rem_hit[:,1],know_hit[:,1],nr_quantiles=4);
+    return optimize.differential_evolution(obj_func,param_bounds);
+
 
 def compute_nll(parameters,remember_hit=remember_hit,know_hit=know_hit,remember_fa=remember_fa,know_fa=know_fa,miss=miss,CR=CR):
     
@@ -155,7 +159,6 @@ def compute_chi(parameters,remember_hit=remember_hit,know_hit=know_hit,remember_
     knowFA_RT,knowFA_conf = numpy.split(know_fa,2,axis=1);
     CR_RT,CR_conf = numpy.split(CR,2,axis=1);
     miss_RT,miss_conf = numpy.split(miss,2,axis=1);
-        
     rem_quantiles_old,know_quantiles_old,data_old = predicted_proportions(c,mu_r,mu_f,d_r,d_f,tc_bound,r_bound,z0,deltaT);
     rem_quantiles_new,know_quantiles_new,data_new = predicted_proportions(c,mu_r0,mu_f0,d_r,d_f,tc_bound,r_bound,z0,deltaT);
     
@@ -200,9 +203,87 @@ def compute_chi_conf(remH,knowH,missH,remFA,knowFA,crFA,rem_quantiles_old,know_q
     chi_conf = chi_old+chi_new;
     return chi_conf;
 
+def compute_model_gof(model_params,rem_RTs,know_RTs,new_RTs,rem_conf,know_conf,nr_quantiles=4):
+    # computes the chi square fit of the model to the data
+    # compute N, the total number of trials
+    N = len(rem_RTs)+len(know_RTs)+len(new_RTs);
+    # compute x, the observed frequency for each category
+    rem_quantiles,know_quantiles,new_quantiles,p_r,p_k,p_n = compute_model_quantiles(model_params,nr_quantiles);
+    # determine the number of confidence levels being used in the model
+    nr_conf_levels = len(rem_quantiles);
+    # adjust the number of confidence levels in the data to match
+    rem_conf = clip(rem_conf,0,nr_conf_levels-1);
+    know_conf = clip(know_conf,0,nr_conf_levels-1);
+    ## compute the number of RTs falling into each quantile bin
+    rem_freqs = array([-diff([sum(rem_RTs[rem_conf==i]>q) for q in rem_quantiles[i]]+[0]) for i in range(nr_conf_levels)]);
+    know_freqs = array([-diff([sum(know_RTs[know_conf==i]>q) for q in know_quantiles[i]]+[0]) for i in range(nr_conf_levels)]);
+    new_freqs = -diff([sum(new_RTs>q) for q in new_quantiles]+[0]);
+    x = hstack([rem_freqs.flatten(),know_freqs.flatten(),new_freqs]);
+    
+    # compute p, the probability associated with each category in the model
+    p_rem = p_r[:,newaxis]*ones((nr_conf_levels,nr_quantiles))/float(nr_quantiles);
+    p_know = p_k[:,newaxis]*ones((nr_conf_levels,nr_quantiles))/float(nr_quantiles);
+    p_new = p_n*ones(nr_quantiles)/float(nr_quantiles);
+    p = hstack([p_rem.flatten(),p_know.flatten(),p_new]);
+    return chi_square_gof(x,N,p)
+
+def compute_model_quantiles(params,nr_quantiles=4):
+    # This function is set up to deal with multiple confidence levels
+    quantile_increment = 1.0/nr_quantiles;
+    quantiles = arange(0,1,quantile_increment);
+    # unpack model parameters
+    # c,mu_r,mu_f,d_r,d_f,tc_bound,r_bound,z0,delta_t = params;
+    # compute marginal distributions
+    p_remember,p_know,p_new,t = predicted_proportions(*params);
+    # compute marginal category proportions (per confidence level)
+    remember_total = p_remember.sum(-1)+EPS;
+    know_total = p_know.sum(-1)+EPS;
+    new_total = p_new.sum()+EPS;
+    # compute integrals of marginal distributions
+    P_r = cumsum(p_remember,-1)/remember_total[:,newaxis];
+    P_k = cumsum(p_know,-1)/know_total[:,newaxis];
+    P_n = cumsum(p_new)/new_total;
+    
+    # compute RT quantiles (by confidence level for know and rem judgments)
+    rem_quantiles = array([t[argmax(P_r>q,-1)] for q in quantiles]).T;
+    know_quantiles = array([t[argmax(P_k>q,-1)] for q in quantiles]).T;
+    new_quantiles = array([t[argmax(P_n>q)] for q in quantiles]);
+    rem_quantiles[:,0] = 0;
+    know_quantiles[:,0] = 0;
+    new_quantiles[0] = 0;
+    # return quantile locations and marginal p(new)
+    return rem_quantiles,know_quantiles,new_quantiles,sum(p_remember,1),sum(p_know,1),sum(p_new);
+
+# def compute_model_quantiles(params,nr_quantiles=4):
+#     quantile_increment = 1.0/nr_quantiles;
+#     quantiles = arange(0,1,quantile_increment);
+#     # unpack model parameters
+#     # c,mu_r,mu_f,d_r,d_f,tc_bound,r_bound,z0,delta_t = params;
+#     # compute marginal distributions
+#     p_remember,p_know,p_new,t = predicted_proportions(*params);
+#     # compute marginal category proportions
+#     remember_total = p_remember.sum()+EPS;
+#     know_total = p_know.sum()+EPS;
+#     new_total = p_new.sum()+EPS;
+#     # compute integrals of marginal distributions
+#     P_r = cumsum(p_remember)/remember_total;
+#     P_k = cumsum(p_know)/know_total;
+#     P_n = cumsum(p_new)/new_total;
+#     
+#     # compute RT quantiles
+#     rem_quantiles = array([t[argmax(P_r>q)] for q in quantiles]);
+#     know_quantiles = array([t[argmax(P_k>q)] for q in quantiles]);
+#     new_quantiles = array([t[argmax(P_n>q)] for q in quantiles]);
+#     rem_quantiles[0] = 0;
+#     know_quantiles[0] = 0;
+#     new_quantiles[0] = 0;
+#     # return quantile locations and marginal p(new)
+#     return rem_quantiles,know_quantiles,new_quantiles,sum(p_remember),sum(p_know),sum(p_new);
+
 def predicted_proportions(c,mu_r,mu_f,d_r,d_f,tc_bound,r_bound,z0,deltaT,use_fftw=True):
     # make c (the confidence levels) an array in case it is a scalar value
     c = array(c,ndmin=1);
+    n = len(c);
     # form an array consisting of the appropriate (upper) integration limits
     clims = hstack(([INF_PROXY],c,[-INF_PROXY]));
     # compute process SD
@@ -230,11 +311,10 @@ def predicted_proportions(c,mu_r,mu_f,d_r,d_f,tc_bound,r_bound,z0,deltaT,use_fft
     else:
         ft_kernel = fft(kernel);
     tx = zeros((len(t),len(x)));
-    #p_know = zeros(shape(t));
-    #p_remember = zeros(shape(t));
+    
+    # Construct arrays to hold RT distributions
     p_old = zeros(shape(t));
     p_new = zeros(shape(t));
-    
     p_rem_conf = zeros((n+1,size(t))); 
     p_know_conf = zeros((n+1,size(t)));
     
@@ -260,8 +340,9 @@ def predicted_proportions(c,mu_r,mu_f,d_r,d_f,tc_bound,r_bound,z0,deltaT,use_fft
     # remove from consideration any particles that already hit the bound
     tx[0]*=(abs(x)<bound[0]);
     
-    # compute the paramteres of the bivariate distribution of particle locations after
-    # deltaT seconds
+    ############################################################################
+    # compute the parameters of the bivariate distribution of particle locations
+    # deltaT seconds after old/new decision
     mu_r_delta = mu_r_cond+mu_r*deltaT;
     mu_comb_delta_c = (mu_r+mu_f)*deltaT;
     s2_r_delta = s_r_cond**2+2*d_r*deltaT;
@@ -269,6 +350,7 @@ def predicted_proportions(c,mu_r,mu_f,d_r,d_f,tc_bound,r_bound,z0,deltaT,use_fft
     cov_delta = s2_r_delta;
     mu_mvn = array([mu_r_delta,mu_comb_delta_c+bound[0]]);
     sigma_mvn = array([[s2_r_delta,cov_delta],[cov_delta,s2_comb_delta]]);
+    ############################################################################
     for j in range(1,len(clims)):
         # Note that the clims appear in descending order, from highest to lowest value
         KLL = array([-INF_PROXY,clims[j]]);     # lower limit for 'know' class
@@ -302,19 +384,10 @@ def predicted_proportions(c,mu_r,mu_f,d_r,d_f,tc_bound,r_bound,z0,deltaT,use_fft
         s_f = sqrt(2*d_f*t[i]);
         # compute STD[r|(r+f)]
         s_r_cond = s_r*sqrt(1-rho**2);
-        #s_f_cond = s_f*sqrt(1-rhoF**2);
         # compute E[r|(r+f)]
         mu_r_cond = mu_r*t[i]+(bound[i]-t[i]*(mu_r+mu_f)-z0)*rho**2;
-        #mu_f_cond = mu_f*t[i]+(x_pos-t[i]*(mu_r+mu_f)-z0)*rhoF**2;
         # remove from consideration any particles that already hit the bound
         tx[i]*=(abs(x)<bound[i]);
-        
-        # old method for computing p_remember
-        # p_remember[i] = sum(p_pos*stats.norm.sf(r_bound,mu_r_cond,s_r_cond));
-        # new method for computing p_remember
-        # mu_r_delta = mu_r_cond+mu_r*deltaT;
-        # s_r_delta = sqrt(s_r_cond**2+2*d_r*deltaT);
-        # p_remember[i] = sum(p_pos*stats.norm.sf(r_bound,mu_r_delta,s_r_delta));
         
         # (6/23/2015) New method for computing p_know, p_remember, and confidences
         # simultaneously using cumulative bivariate normal integral.
@@ -324,8 +397,10 @@ def predicted_proportions(c,mu_r,mu_f,d_r,d_f,tc_bound,r_bound,z0,deltaT,use_fft
         #   2. use the multivariate normal integral function (stats.mvn.mvnun) to compute
         #   the probability that the particle falls into any of the relevant regions
         #   defined by the constant "r" and "conf" bounds
-        # compute the paramteres of the bivariate distribution of particle locations after
-        # deltaT seconds
+        
+        ########################################################################
+        # compute the parameters of the bivariate distribution of particle
+        # locations deltaT seconds after old/new decision
         mu_r_delta = mu_r_cond+mu_r*deltaT;
         mu_comb_delta_c = (mu_r+mu_f)*deltaT;
         s2_r_delta = s_r_cond**2+2*d_r*deltaT;
@@ -333,6 +408,7 @@ def predicted_proportions(c,mu_r,mu_f,d_r,d_f,tc_bound,r_bound,z0,deltaT,use_fft
         cov_delta = s2_r_delta;
         mu_mvn = array([mu_r_delta,mu_comb_delta_c+bound[i]]);
         sigma_mvn = array([[s2_r_delta,cov_delta],[cov_delta,s2_comb_delta]]);
+        ########################################################################
         for j in range(1,len(clims)):
             KLL = array([-INF_PROXY,clims[j]]);
             KUL = array([r_bound,clims[j-1]]);
